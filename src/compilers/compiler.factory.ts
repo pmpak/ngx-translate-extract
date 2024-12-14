73d09fb8f723d0c6250247dfda1252e77ed38196
Panagiotis Bakatselos <github@pmpak.com>
2024-12-14 12:55:58 +0100

    chore: migrate to eslint 9 and update dependencies

--- src/compilers/compiler.factory.ts
+++ src/compilers/compiler.factory.ts
@@ -1,16 +1,16 @@
-import { CompilerInterface } from './compiler.interface.js';
+import { CompilerInterface, CompilerOptions, CompilerType } from './compiler.interface.js';
 import { JsonCompiler } from './json.compiler.js';
 import { NamespacedJsonCompiler } from './namespaced-json.compiler.js';
 import { PoCompiler } from './po.compiler.js';
 
 export class CompilerFactory {
-	public static create(format: string, options?: {}): CompilerInterface {
+	public static create(format: CompilerType, options?: CompilerOptions): CompilerInterface {
 		switch (format) {
-			case 'pot':
-				return new PoCompiler(options);
-			case 'json':
+			case CompilerType.Pot:
+				return new PoCompiler();
+			case CompilerType.Json:
 				return new JsonCompiler(options);
-			case 'namespaced-json':
+			case CompilerType.NamespacedJson:
 				return new NamespacedJsonCompiler(options);
 			default:
 				throw new Error(`Unknown format: ${format}`);
--- src/compilers/compiler.interface.ts
+++ src/compilers/compiler.interface.ts
@@ -1,5 +1,15 @@
 import { TranslationCollection } from '../utils/translation.collection.js';
 
+export enum CompilerType {
+	Pot = 'pot',
+	Json = 'json',
+	NamespacedJson = 'namespaced-json',
+}
+
+export interface CompilerOptions {
+	indentation?: string;
+}
+
 export interface CompilerInterface {
 	extension: string;
 
